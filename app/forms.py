b17d5b6e44ac65e43bb548e296f674df6b77c6ff
def <srbhatia10@gmail.com>
2014-05-19 22:22:04 +0530

    Add comment to LoginForm describing class' purpose

--- app/forms.py
+++ app/forms.py
@@ -3,5 +3,8 @@ from wtforms import TextField, BooleanField
 from wtforms.validators import Required
 
 class LoginForm(Form):
+  """
+  Form for sign in page
+  """
   openid = TextField('openid', validators=[Required()])
   remember_me = BooleanField('remember_me', default=False)
