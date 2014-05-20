27ff37be4de6216a79c14dfee9b041ef1778656a
def <srbhatia10@gmail.com>
2014-05-20 08:53:00 +0530

    Add support for OpenID authentication

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 # Handlers that respond to requests from browsers
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, session, url_for, g, request
+from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
 from forms import LoginForm
 from models import User, ROLE_USER, ROLE_ADMIN
@@ -23,22 +24,67 @@ def index():
   return render_template('index.html', title='Home', user=user, posts=posts)
 
 
+@app.before_request
+def before_request():
+  """
+  Runs before the view function
+  Sets the current user to the global user for better access
+  """
+  # current_user is set by login_user of flask-login
+  g.user = current_user
+
+
 @app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler  # tell Flask-OpenID that this is our login view function
 def login():
   """
   Login form for signing into app
   """
+  # Do not present login page if already signed in
+  if g.user is not None and g.user.is_authenticated():
+    return redirect(url_for('index'))
+  # Present login form
   form = LoginForm()
-  # form processing upon submission
-  # if any field fails validation, False is returned & form is re-rendered
+  # process form fields upon submission
   if form.validate_on_submit():
-    msg = 'Login requested for OpenID: "' + form.openid.data + '", remember_me: ' +\
-     str(form.remember_me.data)
-    flash(msg)  # display logs
-    return redirect('/index')  # redirect to index page after login
-  # render the form page
-  return render_template('login.html', title='Sign In', form=form,
-          providers=app.config['OPENID_PROVIDERS'])
+    session['remember_me'] = form.remember_me.data
+    # Try login with Open-ID
+    # Query OpenID provider for user's nickname and email
+    return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
+  # render the form
+  render_template('login.html', title='Sign In', form=form,
+                  providers=app.config['OPENID_PROVIDERS'])
+
+
+@oid.after_login
+def after_login(resp):
+  """
+  Callback after authentication using Flask-OpenID
+
+  Args:
+    resp: response from OpenID provider
+  """
+  if resp.email is None or resp.email == '':
+    flash('Invalid login. Please try again.')
+    return redirect(url_for('login'))
+  user = User.query.filter_by(email=resp.email).first()
+  if user is None:
+    # no such user, create a new user
+    nickname = resp.nickname
+    if nickname is None or nickname == '':
+      # could not get nickname from OpenID provider
+      # get a nickname from the email address
+      nickname = resp.email.split('@')[0]
+    # create a new user and add to database
+    user = User(nickname=nickname, email=resp.email, role=ROLE_USER)
+    db.session.add(user)
+    db.session.commit()
+  remember_me = False
+  if 'remember_me' in session:
+    remember_me = session['remember_me']
+    session.pop('remember_me', False)
+  login_user(user, remember_me=remember_me)  # register this is a valid login
+  return redirect(request.args.get('next') or url_for('index'))
 
 
 @lm.user_loader
