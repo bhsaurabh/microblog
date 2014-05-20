168b51486009e49954e18ae3ee35c11eedf8f6fd
def <srbhatia10@gmail.com>
2014-05-20 07:50:00 +0530

    Add Post model and link to User

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import db
 ROLE_USER = 0
 ROLE_ADMIN = 1
 
-class User:
+class User(db.Model):
   """
   Model for a User in the microblog application
   """
@@ -11,6 +11,7 @@ class User:
   nickname = db.Column(db.String(64), index=True, unique=True)
   email = db.Column(db.String(120), index=True, unique=True)
   role = db.Column(db.SmallInteger, default=ROLE_USER)
+  posts = db.relationship('Post', backref='author', lazy='dynamic')
 
   def __repr__(self):
     """
@@ -20,3 +21,22 @@ class User:
       A string representation of the user
     """
     return '<User %r>' % (self.nickname)
+
+
+class Post(db.Model):
+  """
+  Model for a Post in the microblog application
+  """
+  id = db.Column(db.Integer, primary_key=True)
+  body = db.Column(db.String(140))
+  timestamp = db.Column(db.DateTime)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+  def __repr__(self):
+    """
+    String representation of the post. Useful in printing
+
+    Returns:
+      A string representation of the post
+    """
+    return '<Post %r'> % (self.body)
--- config.py
+++ config.py
@@ -15,4 +15,4 @@ OPENID_PROVIDERS = [
 
 # SQLALCHEMY config
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATION_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
