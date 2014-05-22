edd8a811283610186b44510edf9cd9f8571d11da
def <srbhatia10@gmail.com>
2014-05-22 14:19:48 +0530

    Add support for local timezones

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,9 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from config import basedir
-import os
+import osfrom momentjs import momentjs
+
+app.jinja_env.globals['momentjs'] = momentjs
 
 app = Flask(__name__)
 app.config.from_object('config')  # read-in configuration file
