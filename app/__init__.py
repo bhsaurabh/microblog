0f5d3bfd5256bcadfa288356f24cba12affe3585
def <srbhatia10@gmail.com>
2014-05-22 14:21:13 +0530

    Fix import bugs

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,11 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from config import basedir
-import osfrom momentjs import momentjs
-
-app.jinja_env.globals['momentjs'] = momentjs
+import os
+from momentjs import momentjs
 
 app = Flask(__name__)
+app.jinja_env.globals['momentjs'] = momentjs
 app.config.from_object('config')  # read-in configuration file
 db = SQLAlchemy(app)
 
